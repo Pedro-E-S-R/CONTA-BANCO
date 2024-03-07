e16360403e3c9850029931e088071389cbb89006
Pedro Rocha <pedr012012@outlook.com>
2024-03-07 20:39:08 -0300

    finalizado

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        }
+        int numero;
+        String agencia;
+        String nome_cliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da Agência !");
+        numero = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Por favor, digite a Agência !");
+        agencia = sc.nextLine();
+        System.out.println("Por favor, digite o seu nome !");
+        nome_cliente = sc.nextLine();
+        System.out.println("Por favor, digite o seu saldo !");
+        saldo = sc.nextDouble();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco," +
+                " sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque"
+        ,nome_cliente,agencia,numero,saldo);
+        System.out.println();
     }
 }
